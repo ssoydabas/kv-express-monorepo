2d8558a290c9d20d904f9dca011f5c9ee9ae655f
Sertan Soydabas <ssoydabas41@gmail.com>
2023-08-03 15:02:57 +0300

    feat(mobile-app): adds mobile app with a single page

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  overrides: [
+    {
+      files: ["**/*.ts", "**/*.tsx"],
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "standard-with-typescript",
+        "plugin:prettier/recommended",
+      ],
+      rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/strict-boolean-expressions": [
+          "error",
+          {
+            allowString: true,
+            allowNullableString: true,
+          },
+        ],
+      },
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: [
+          "./packages/apps/*/tsconfig.json",
+        ],
+      },
+    },
+  ],
+};
