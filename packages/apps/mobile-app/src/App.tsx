3b432875886f0f88a7db0b5b62fab55c860a5ec7
Sertan Soydabas <ssoydabas41@gmail.com>
2023-08-11 16:35:18 +0300

    WIP

--- packages/apps/mobile-app/src/App.tsx
+++ packages/apps/mobile-app/src/App.tsx
@@ -2,9 +2,6 @@ import ServicesProvider from "./services/ServicesProvider";
 import Navigation from "./navigation/Navigation";
 
 export default function App() {
-  console.log("ENV VARS!!!");
-  console.log(process.env);
-
   return (
     <ServicesProvider>
       <Navigation />
