8c813827fa8f942c55bc718d2938038cb5e14da8
Sertan Soydabas <ssoydabas41@gmail.com>
2023-08-11 16:02:52 +0300

    wip

--- packages/apps/api/src/api/middlewares/corsMiddleware.ts
+++ packages/apps/api/src/api/middlewares/corsMiddleware.ts
@@ -1,5 +1,11 @@
 import cors from "cors";
 
-const corsMiddleware = cors();
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+const corsMiddleware = cors(corsOptions);
 
 export default corsMiddleware;
